9b5dd5424797f0f79ffb8e1edb3eddea4827e7c6
Da Def <da.def.lang.belta@gmail.com>
2025-12-08 17:01:38 +0100

    wip full grammar

--- bin/main.ml
+++ bin/main.ml
@@ -4,8 +4,13 @@ let () =
   let () = Random.self_init() in
   let digits = List.map trm ["0";"1";"2";"3"] in
   let alphas = List.map trm ["a";"b";"c"] in
-  print_endline@@ pick 8 @@ leq @@ seqs [
+  print_endline @@ pick 8 [] @@ leq @@ seqs [
     alts [Seq(seqs digits, trm "a"); Seq(trm "0", seqs alphas)];
     trm ":";
     alts digits;
-    alts alphas]
+    alts alphas];
+  let xpr = seqs [ alts [ alts digits; ref "sum" ];
+                   trm "*";
+                   alts [ alts digits; ref "sum" ] ] in
+  let sum = seqs [ ref "xpr"; trm "+"; ref "xpr" ] in
+  print_endline @@ pick 20 [("xpr",xpr);("sum",sum)] @@ leq @@ xpr
--- lib/lib.ml
+++ lib/lib.ml
@@ -3,6 +3,7 @@ type re =
   | Alt of re * re
   | Seq of re * re
   | Leq of re
+  | Ref of string * (int array)
 
 let split xs =
   let m = (List.length xs) / 2 in
@@ -19,33 +20,39 @@ let trm s = Trm s
 let alts  = tfold (fun x y -> Alt (x,y))
 let seqs  = tfold (fun x y -> Seq (x,y))
 let leq x = Leq x
+let ref s = Ref (s, let a = Array.make 32 (-1) in (Array.set a 0 0; a))
 
-let rec ways n r =
+let rec ways n r v =
   match r with
   | Trm _     -> if n = 1 then 1 else 0
-  | Alt (x,y) -> (ways n x) + (ways n y)
-  | Seq (x,y) -> sum n (fun i -> (ways i x) * (ways (n-i) y)) 0
-  | Leq x     -> sum n (fun i -> ways i x) 0
-and sum n f a = if n < 0 then a else sum (n-1) f (a + f n)
+  | Alt (x,y) -> (ways n x v) + (ways n y v)
+  | Seq (x,y) -> sum (n-1) (fun i -> (ways i x v) * (ways (n-i) y v)) 0
+  | Leq x     -> sum (n-1) (fun i -> ways i x v) 0
+  | Ref (s,a) -> let c = Array.get a n in
+                 if c <> -1 then c else
+                 (let u = ways n (List.assoc s v) v in
+                  Array.set a n u; u)
+and sum n f a = if n < 1 then a else sum (n-1) f (a + f n)
 
-let rec kth n k r =
+let rec kth n k r v =
   match r with
   | Trm s     -> if n = 1 then s else "*barf*"
-  | Alt (x,y) -> let lefts = ways n x in
-                 if k < lefts then (kth n  k        x)
-                              else (kth n (k-lefts) y)
-  | Seq (x,y) -> kth_seq n k (x,y) 0
-  | Leq x     -> kth_leq n k x 0 
-and kth_seq n k (x,y) m =
-  let more = (ways m x)*(ways (n-m) y) in
-  if k < more then let d = ways m x in
-                   (kth m (k mod d) x) ^ " " ^ (kth (n-m) (k / d) y)
-              else (kth_seq n (k-more) (x,y) (m+1))
-and kth_leq n k x m =
-  let more = (ways m x) in
-  if k < more then kth m k x
-              else kth_leq n (k-more) x (m+1)
+  | Alt (x,y) -> let lefts = ways n x v in
+                 if k < lefts then (kth n  k        x v)
+                              else (kth n (k-lefts) y v)
+  | Seq (x,y) -> kth_seq n k (x,y) v 0
+  | Leq x     -> kth_leq n k x v 0
+  | Ref (s,_) -> kth n k (List.assoc s v) v
+and kth_seq n k (x,y) v m =
+  let more = (ways m x v)*(ways (n-m) y v) in
+  if k < more then let d = ways m x v in
+                   (kth m (k mod d) x v) ^ " " ^ (kth (n-m) (k / d) y v)
+              else (kth_seq n (k-more) (x,y) v (m+1))
+and kth_leq n k x v m =
+  let more = (ways m x v) in
+  if k < more then kth m k x v
+              else kth_leq n (k-more) x v (m+1)
 
-let pick n r =
-  let k = Random.int @@ ways n r in
-  kth n k r
+let pick n v r =
+  let k = Random.int @@ ways n r v in
+  kth n k r v
